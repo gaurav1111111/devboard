01836035a79411fd3c000d68a4ca8b1348a07842
Gaurav Sharma <gs12242@gmail.com>
2026-08-13 01:00:38 +0000

    Added nginx script

---
+++ install-nginx.sh
@@ -0,0 +1,16 @@
+#!/bin/ bash
+
+sudo apt-get update
+sudo apt-get install nginx -y
+
+sudo systemctl start nginx
+sudo systemctl enable nginx
+
+cp hello.html /var/www/html
+
+sudo systemctl restart nginx
+
+echo "Devboard running at port 80"
+
+
+
